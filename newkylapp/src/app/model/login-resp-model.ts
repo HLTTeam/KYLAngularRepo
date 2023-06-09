73ded890582717fe3d5d4f261918961a0ee9d8bf
HLTTeam <134317468+HLTTeam@users.noreply.github.com>
2023-06-09 15:08:41 +0530

    Create login-resp-model.ts
    Added new model file

---
+++ newkylapp/src/app/model/login-resp-model.ts
@@ -0,0 +1,11 @@
+export class LoginRespModel{
+    id: string | undefined;
+    name: string | undefined; 
+    email:string | undefined;
+    phone: number | undefined;
+    address:string | undefined;
+    incomeType: string | undefined;
+    income: number | undefined;
+    expenses: number | undefined;
+    loanIds: string[] | undefined;
+}
