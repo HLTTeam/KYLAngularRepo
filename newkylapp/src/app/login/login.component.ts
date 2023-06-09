e590ca1f182739f7fdf0985f2feb3defcb8dbba0
madduseshavenkata.p <madduseshavenkata.p@mphasis.com>
2023-06-09 11:32:53 +0530

    Commit the changes after login servcie integration

--- newkylapp/src/app/login/login.component.ts
+++ newkylapp/src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginModel } from '../model/login-model';
 import { LoginService } from '../service/login.service';
+import { LoginRespModel } from '../model/login-resp-model';
+
 
 @Component({
   selector: 'app-login',
@@ -14,7 +16,11 @@ export class LoginComponent implements OnInit{
   submitted = false;
   email:string | undefined;
   password:string | undefined;
+  userName:string | undefined;
   loginModel: LoginModel = new LoginModel;
+  loginRespModel: LoginRespModel = new LoginRespModel;
+  bearerToken : String = "Bearer asdfas342";
+  userProfileID :string | undefined;
   constructor(private fb:FormBuilder,private router:Router,public loginservice:LoginService){}
   ngOnInit(): void 
   {
@@ -36,14 +42,33 @@ export class LoginComponent implements OnInit{
           localStorage.setItem("email",this.loginForm.get('email')?.value);
           localStorage.setItem("password",this.loginForm.get('password')?.value);  
           this.email=this.loginForm.get('email')?.value;
+          this.userName=this.loginForm.get('email')?.value;
           this.password=this.loginForm.get('password')?.value;
           this.loginModel.email=this.email;
           this.loginModel.password=this.password;
+          this.loginModel.userName=this.userName;
           this.clear();
           this.loginForm.disable();
-          
-
-         this.router.navigate(['loandetails']);
+          this.loginservice.login(this.loginModel).subscribe(data => {
+            this.loginRespModel = data;
+            this.userProfileID = this.loginRespModel['id'];
+            console.log(this.userProfileID);
+            localStorage.setItem("userProfileID",this.userProfileID as string);  
+          }, resErr => {
+            console.log("err mess" +resErr);
+          });
+          console.log(this.loginRespModel);
+          console.log(JSON.stringify(this.loginRespModel));
+          console.log((localStorage.getItem("userProfileID") ));
+          console.log((this.userProfileID != null));
+          if (localStorage.getItem("userProfileID") != null){
+            console.log("UserProfileID is not null");
+            this.router.navigate(['loandetails']);
+          }else{
+          console.log("UserProfileID is  null");
+          //this.router.navigate(['login']);
+          this.router.navigate(['login']);
+          }
        }
   }
   clear()
--- newkylapp/src/app/model/login-model.ts
+++ newkylapp/src/app/model/login-model.ts
@@ -1,7 +1,7 @@
 export class LoginModel{
 
     email:string | undefined;
-
+    userName:string | undefined;
     password:string | undefined;
 
 
